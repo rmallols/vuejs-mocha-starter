0e81b73d89c0e40df6eb0db207c882122e13d158
Ricardo Mallols <ricardo.mallols@gmail.com>
2020-04-03 17:36:57 +0100

    Adding support for async / await on the tests

--- test/Counter.spec.js
+++ test/Counter.spec.js
@@ -2,17 +2,11 @@ import Vue from 'vue';
 import { shallowMount } from '@vue/test-utils';
 import Counter from '../src/Counter.vue';
 
-// describe('Counter.vue', () => {
-//     it('increments count when button is clicked', async () => {
-//         const wrapper = shallowMount(Counter);
-//         wrapper.find('button').trigger('click');
-//         await Vue.nextTick();
-//         expect(wrapper.find('div').text()).toMatch('1');
-//     })
-// })
-
-describe('test', () => {
-    it('test!', () => {
-        expect(true).toBe(true);
+describe('Counter.vue', () => {
+    it('increments count when button is clicked', async () => {
+        const wrapper = shallowMount(Counter);
+        wrapper.find('button').trigger('click');
+        await Vue.nextTick();
+        expect(wrapper.find('div').text()).toMatch('1');
     })
-})
+});
